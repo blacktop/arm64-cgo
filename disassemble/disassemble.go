0140c5a4f3d1074ddf2621c7e3472bb62fa5e970
blacktop <blacktop@users.noreply.github.com>
2021-07-17 15:11:29 -0600

    Update disassemble.go

--- disassemble/disassemble.go
+++ disassemble/disassemble.go
@@ -24,6 +24,7 @@ import (
 	"fmt"
 )
 
+// GetOpCodeByteString returns the opcodes as a string of hex bytes
 func GetOpCodeByteString(opcode uint32) string {
 	op := new(bytes.Buffer)
 	err := binary.Write(op, binary.LittleEndian, opcode)
@@ -36,17 +37,18 @@ func GetOpCodeByteString(opcode uint32) string {
 // Disassemble disassembles an instruction
 func Disassemble(addr uint64, instruction uint32, verbose bool) (string, error) {
 
+	var results []byte
+	cResults := (*C.char)(C.CBytes(results))
+
 	instrBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(instrBytes, instruction)
 
-	var results []byte
-	resultsPointer := C.CBytes(results)
-	cResults := (*C.char)(resultsPointer)
-
-	dataPointer := C.CBytes(instrBytes)
-	cData := (*C.uint8_t)(dataPointer)
-
-	C.disassemble(C.uint64_t(addr), cData, C.int(4), cResults, C.bool(verbose))
+	C.disassemble(
+		C.uint64_t(addr),                   // uint64_t addr
+		(*C.uint8_t)(C.CBytes(instrBytes)), // uint8_t *data
+		C.int(4),                           // int len
+		cResults,                           // char *result
+		C.bool(verbose))                    // bool verbose
 
 	return C.GoString(cResults), nil
 }
