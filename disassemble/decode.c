4574572a657165db33541e48640a2efdb5541870
blacktop <blacktop@users.noreply.github.com>
2021-10-31 17:43:39 -0600

    chore: update with recent upstream changes

--- disassemble/decode.c
+++ disassemble/decode.c
@@ -1,13 +1,13 @@
 #include "decode.h"
 #include "feature_flags.h"
 
-int decode_spec(context *ctx, Instruction *dec);	   // from decode0.cpp
-int decode_scratchpad(context *ctx, Instruction *dec); // from decode_scratchpad.c
+int decode_spec(context* ctx, Instruction* dec);        // from decode0.cpp
+int decode_scratchpad(context* ctx, Instruction* dec);  // from decode_scratchpad.c
 
-int aarch64_decompose(uint32_t instructionValue, Instruction *instr, uint64_t address)
+int aarch64_decompose(uint32_t instructionValue, Instruction* instr, uint64_t address)
 {
 	context ctx = {0};
-	ctx.halted = 1; // enable disassembly of exception instructions like DCPS1
+	ctx.halted = 1;  // enable disassembly of exception instructions like DCPS1
 	ctx.insword = instructionValue;
 	ctx.address = address;
 	ctx.features0 = ARCH_FEATURES_ALL;
@@ -31,7 +31,7 @@ int aarch64_decompose(uint32_t instructionValue, Instruction *instr, uint64_t ad
 	}
 
 	/* if UDF encoding, return undefined */
-	//if(instr->encoding == ENC_UDF_ONLY_PERM_UNDEF)
+	// if(instr->encoding == ENC_UDF_ONLY_PERM_UNDEF)
 	//	return DECODE_STATUS_UNDEFINED;
 
 	/* convert the pcode variables to list of operands, etc. */
